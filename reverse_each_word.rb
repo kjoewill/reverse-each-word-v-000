522b467741fcca644b9fb2d87a155294598eac5a
Kevin W <kjoewill@gmail.com>
2018-08-21 19:45:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence)
   split_array = sentence.split
   reversed_array = []
   split_array.each { |e| reversed_array << e.reverse }
-  reversed_array
+  reversed_array.join(" ")
 end
 
 

2cdaa97da454b541a3bc9988c5aa38712e912046
Kevin W <kjoewill@gmail.com>
2018-08-21 19:47:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
 some_sentence = "This is, some sentence"
 
-def reverse_each_word(sentence)
+def reverse_each_word2(sentence)
   split_array = sentence.split
   reversed_array = []
   split_array.each { |e| reversed_array << e.reverse }
   reversed_array.join(" ")
 end
 
-def reverse_each_word2(sentence)
+def reverse_each_word(sentence)
   split_array = sentence.split
   reversed_array = split_array.collect { |e| e.reverse }
   reversed_array.join(" ")
@@ -17,4 +17,4 @@ end
 
 
 
-puts reverse_each_word2(some_sentence).inspect
+puts reverse_each_word(some_sentence).inspect
